e59d94522d8c95665ee3d54afb9400c892e699c1
Natsuki-Ike <ctppag@outlook.jp>
2020-09-06 12:47:57 +0900

    herokuデプロイのためcarrierwaveの編集

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,10 +1,14 @@
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
+
 unless Rails.env.development? || Rails.env.test?
   CarrierWave.configure do |config|
     config.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
       aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-      region: 'ap-northeast-1'
+      region: ENV['AWS_S3_REGION']
     }
 
     config.fog_directory  = 'instalike-photo'

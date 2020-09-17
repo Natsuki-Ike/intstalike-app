410838f08bf22be574b1ba42457de3a924a115b8
Natsuki-Ike <ctppag@outlook.jp>
2020-09-17 10:42:36 +0900

    メール認証機能の削除

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,10 +109,10 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-  
+
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'instalike914.herokuapp.com'
+  host = 'instalike917.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
